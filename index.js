5c050bfaa200cdc59b7dfab0f4bc004d17f377bb
Sirajul Islam <ayasabegum134@gmail.com>
2023-08-20 02:43:22 +0600

    my total price done

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
+// total cost
 
-
+let total = 0;
 function toTouch(target){
-    const itemName = target.childNodes[1].childNodes[11].innerText;
-    const h3 = document.createElement('h3');
-    h3.innerText = itemName;
-    const receiveProperty = document.getElementById('send-first-item-name');
-    receiveProperty.appendChild(h3)
+    const priceTag = target.childNodes[1].childNodes[13].childNodes[0].innerText;
+    const price = parseFloat(priceTag);
+    total = total + price;
+    const searchPrice = document.getElementById('total-price-output');
+    searchPrice.innerText = total;
 }
 
+// first section item name send to order
 function toTouch2(target2){
     const itemName = target2.childNodes[1].childNodes[11].innerText;
     const h3 = document.createElement('h3');
     h3.innerText = itemName;
-    const receiveProperty = document.getElementById('send-second-item-name');
-    receiveProperty.appendChild(h3)
+    const receiveProperty = document.getElementById('send-first-item-name');
+    receiveProperty.appendChild(h3);
 }
-
+// second section item name send to order
 function toTouch3(target3){
     const itemName = target3.childNodes[1].childNodes[11].innerText;
     const h3 = document.createElement('h3');
     h3.innerText = itemName;
+    const receiveProperty = document.getElementById('send-second-item-name');
+    receiveProperty.appendChild(h3);
+}
+// third section item name send to order
+function toTouch4(target4){
+    const itemName = target4.childNodes[1].childNodes[11].innerText;
+    const h3 = document.createElement('h3');
+    h3.innerText = itemName;
     const receiveProperty = document.getElementById('send-third-item-name');
-    receiveProperty.appendChild(h3)
+    receiveProperty.appendChild(h3);
 }
